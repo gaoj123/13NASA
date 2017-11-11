035a4c875c75051fdfdb3e017e11794fef6cf8b6
Jenny Gao <jgao658@gmail.com>
2017-11-10 21:58:22 -0500

    tested out census api

--- app.py
+++ app.py
@@ -9,7 +9,14 @@ def root():
     data_string=u.read()
     j=json.loads(data_string)
     return render_template("base.html", imgURL=j["url"], text=j["explanation"], title=j["title"], cr=j["copyright"])
-                                               
+
+@app.route("/census")
+def census():
+    v=urllib2.urlopen("https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE=7&for=state:*&key=174dd7f6a223bf67465afb00579436323ade263d")
+    data_v=v.read()
+    j2=json.loads(data_v)
+    return render_template("census.html", data=j2)
+
 if __name__=="__main__":
     app.debug=True
     app.run()
